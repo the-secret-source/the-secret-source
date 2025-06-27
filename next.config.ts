699237655c2b8326668575f6df56f97e5c71c0d3
Karn Watcharasupat <kwatcharasupat@gmail.com>
2025-06-27 22:53:16 +0000

    Do not ever try to implement the icon component. Use the  assets/icons f

--- next.config.ts
+++ next.config.ts
@@ -19,13 +19,31 @@ const nextConfig: NextConfig = {
     ],
   },
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    });
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg'),
+    )
 
-    return config;
+    config.module.rules.push(
+      // Re-apply the default rule, and exclude SVG imports handled by SVGR
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      }
+    )
+
+    // Modify the original rule to ignore SVGs handled by SVGR
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
   },
 };
 
